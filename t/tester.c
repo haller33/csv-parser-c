02e76ecda720d99562279e98a95e5159f6485a25
haller33 <eliseu42lucena@gmail.com>
2023-06-29 19:33:14 -0300

    some interators example for testing and documentation

--- t/tester.c
+++ t/tester.c
@@ -33,6 +33,54 @@ main(int argc, char *argv[]) {
 
   csvc_free_context(ctx_neo);
 
+  /**
+   *
+   * On this example, we interate over a one column of
+   * the csv file.
+   * this is done to simplify the need to use another variable
+   *
+   */
+  csv_adt *ctx = csvc_init_read_file_path(file_name_path);
+
+  while (csvc_interate_increase_file(ctx)) {
+    char *current_chr = csvc_current_cell_by_index_of_column(ctx, 0);
+
+    printf("%s\n", current_chr);
+  }
+
+  csvc_free_context(ctx);
+
+
+  ///
+  /// Example with multiples interators at same time.
+  ///
+
+  csv_adt *ctx_two = csvc_init_read_file_path(file_name_path);
+
+  size_t relative_count = 0;
+  while (csvc_interate_increase_file(ctx_two)) {
+
+    if ((relative_count % 2) == 0) {
+      char **current_arr = csvc_current_line(ctx_two);
+
+      size_t num_line = csvc_current_line_number(ctx_two);
+
+      size_t item = atoi(current_arr[1]) / num_line ;
+
+      printf("second column %d\n", (int)item);
+    } else {
+      /// you cannot call the same interator at the same time
+      //  without refreshing interate_increase_file
+      //
+      char *current_chr = csvc_current_cell_by_index_of_column(ctx_two, 0);
+
+      printf("first column %d\n", atoi(current_chr));
+    }
+
+    relative_count += 1;
+  }
+
+  csvc_free_context(ctx);
 
 
   return 0;
